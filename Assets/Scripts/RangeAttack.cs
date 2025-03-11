76280b38bed92134a1644b8fff83cf436afc3e47
quanNMHE170463 <quanNMHE170463@fpt.edu.vn>
2025-03-11 21:30:36 +0700

    Message 9:30

---
+++ Assets/Scripts/RangeAttack.cs
@@ -0,0 +1,32 @@
+﻿using Pathfinding;
+using UnityEngine;
+
+public class EnemyRangeAttack : MonoBehaviour
+{
+    public AIPath aiPath; // Tham chiếu đến AIPath của Enemy
+    private bool playerInRange = false;
+
+    void Start()
+    {
+        aiPath.canMove = false; // Mặc định không cho Enemy di chuyển
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            aiPath.canMove = true; // Khi Player vào phạm vi -> Cho phép di chuyển
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            aiPath.canMove = false; // Khi Player rời phạm vi -> Dừng di chuyển
+        }
+    }
+}
+
